f6550c9414f0f2a4639c2be7a0130a6687c950d9
ChocoParrot <lachocoparrot@gmail.com>
2019-01-11 23:17:46 +0800

    [refactor] changed humourme to joke, added typing intermediate

--- systems/commands_memes/cat.js
+++ systems/commands_memes/cat.js
@@ -5,8 +5,11 @@ module.exports = async function (message, params, config) {
 
   var loading = await message.channel.send(":cat: Performing CPR on a cat. Please wait.");
 
+  message.channel.startTyping();
+
   var attachment = new Discord.Attachment(endpoint, "cat.gif");
 
   await message.channel.send("", attachment);
-  loading.delete();
+  await loading.delete();
+  await message.channel.stopTyping();
 };
--- systems/commands_memes/joke.js
+++ systems/commands_memes/joke.js
@@ -3,8 +3,11 @@ var request = require("request-promise");
 module.exports = async function (message, params, config) {
   var endpoint = "https://icanhazdadjoke.com/";
 
+  message.channel.startTyping();
+
   var joke = await request({uri: endpoint, headers: {Accept: "text/plain"}});
 
-  message.channel.send(":bamboo: " + joke);
+  await message.channel.send(":bamboo: " + joke);
+  await message.channel.stopTyping();
 
 };
