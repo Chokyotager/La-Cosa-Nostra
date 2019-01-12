d1bfd3d2fb63eb81d9217d319c6a806a37923f37
ChocoParrot <lachocoparrot@gmail.com>
2019-01-12 22:38:31 +0800

    [refactor][critical] order of dependencies

--- source/lcn.js
+++ source/lcn.js
@@ -1,15 +1,13 @@
-module.exports = {
+// NOTE: the order matters!
 
-  auxils: require("./systems/auxils.js"),
-  rolesystem: require("./rolesystem/rolesystem.js"),
-  expansions: require("./systems/expansions.js"),
-  executable: require("./systems/executable.js"),
-  commands: require("./systems/commands.js"),
-  game: require("./systems/game.js"),
-  flavours: require("./systems/flavours.js"),
-  expansions: require("./systems/expansions.js"),
-  win_conditions: require("./systems/win_conditions.js"),
-  attributes: require("./systems/attributes.js"),
-  actionables: require("./systems/actionables.js")
-
-}
+module.exports.auxils = require("./systems/auxils.js");
+module.exports.rolesystem = require("./rolesystem/rolesystem.js");
+module.exports.expansions = require("./systems/expansions.js");
+module.exports.attributes = require("./systems/attributes.js");
+module.exports.executable = require("./systems/executable.js");
+module.exports.commands = require("./systems/commands.js");
+module.exports.game = require("./systems/game.js");
+module.exports.flavours = require("./systems/flavours.js");
+module.exports.expansions = require("./systems/expansions.js");
+module.exports.win_conditions = require("./systems/win_conditions.js");
+module.exports.actionables = require("./systems/actionables.js");
