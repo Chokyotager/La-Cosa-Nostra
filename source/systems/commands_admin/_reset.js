49f416ecba68dff1bcafed75bca2b640e8567a1b
ChocoParrot <lachocoparrot@gmail.com>
2019-02-15 22:05:52 +0800

    [refactor] reset command pending message

--- source/systems/commands_admin/_reset.js
+++ source/systems/commands_admin/_reset.js
@@ -2,6 +2,8 @@ var reset = require("../game_reset/reset.js");
 
 module.exports = async function (message, params, config) {
 
+  await message.channel.send(":hourglass_flowing_sand: Resetting, please wait. This may take a while.");
+
   await reset(message.client, config);
 
   await message.channel.send(":ok: Game reset.");
