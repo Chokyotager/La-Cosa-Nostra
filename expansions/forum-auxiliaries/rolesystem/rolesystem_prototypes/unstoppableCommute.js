db2ee0cedec4471d5947de0346fc075ce26c9df5
ChocoParrot <lachocoparrot@gmail.com>
2019-04-17 18:06:41 +0800

    [factor] commutation forum-auxiliaries rolesystem

---
+++ expansions/forum-auxiliaries/rolesystem/rolesystem_prototypes/unstoppableCommute.js
@@ -0,0 +1,51 @@
+module.exports = function (actionable, game, params, notify=false, consider_roleblocked=true) {
+
+  var target = game.getPlayerByIdentifier(actionable.to);
+
+  // Seen as visit
+  game.execute("visit", {visitor: actionable.from,
+    target: actionable.to,
+    priority: actionable.priority});
+
+  // Stops all actions to the target that are roleblockable
+  var actions = game.actions.findAll(x => (x.to === actionable.to || x.to === actionable.target) && x.tags.includes("roleblockable") && !x.tags.includes("permanent") && x.execution <= 0);
+
+  if (notify) {
+    for (var i = 0; i < actions.length; i++) {
+      // Inform of failure
+
+      var action = actions[i];
+
+      var affected = game.getPlayerByIdentifier(action.from);
+
+      game.addMessage(affected, ":exclamation: Your action failed because your target was " + module.exports.reason + "!");
+
+    };
+  };
+
+  if (consider_roleblocked) {
+    for (var i = 0; i < actions.length; i++) {
+      // Inform of failure
+
+      var action = actions[i];
+
+      // Consider user roleblocked by default
+      game.execute("roleblock", {roleblocker: actionable.from,
+        target: action.from,
+        priority: actionable.priority,
+        reason: "Commuter-roleblock"});
+
+    };
+
+  };
+
+  target.setStatus("kidnapped", true);
+
+  game.actions.delete(x => x.from === actionable.to && x.tags.includes("visit"));
+  game.actions.delete(x => (x.to === actionable.to || x.to === actionable.target) && x.tags.includes("visit") && !x.tags.includes("permanent") && x.execution <= 0);
+
+  return true;
+
+};
+
+module.exports.reason = "away";
