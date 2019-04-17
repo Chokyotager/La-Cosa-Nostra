3c702accd07a94adee7fbca51157124e26669468
ChocoParrot <lachocoparrot@gmail.com>
2019-04-17 14:50:28 +0800

    [refactor] updated credits command

--- source/systems/commands_unaffiliated/credits.js
+++ source/systems/commands_unaffiliated/credits.js
@@ -1,6 +1,6 @@
 module.exports = function (message, params, config) {
 
-  message.channel.send(":fireworks: :fireworks: :fireworks:   **La Cosa Nostra Credits**   :fireworks: :fireworks: :fireworks:\n\n**Contributors**: ChocoParrot, Hex4Nova\n\n**Alpha-testers**: Justin, Lia\n\n**Beta-testers**: ShapeShifted, Les, Lia, Misode, Conjurer, Good Skele, Hex4Nova, Inffy\n\n**Special thanks**: Error, Fogalog, Inffy, Randium");
+  message.channel.send(":fireworks: :fireworks: :fireworks:   **La Cosa Nostra Credits**   :fireworks: :fireworks: :fireworks:\n\n**Contributors**: ChocoParrot, Hex4Nova\n\n**Alpha-testers**: Justin, Lia\n\n**Beta-testers**: Ardvark8op, ShapeShifted, Les, Lia, Misode, Conjurer, Good Skele, Hex4Nova, Inffy\n\n**Special thanks**: Error, Fogalog, Inffy, Randium, Sai Kurogetsu, Alisha");
   return null;
 
 };
