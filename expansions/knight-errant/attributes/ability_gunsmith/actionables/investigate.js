a624a1f00ff17514e25971c87510d28631018ae8
ChocoParrot <lachocoparrot@gmail.com>
2019-04-19 12:29:37 +0800

    [refactor] Knight-Errant gunsmith ability decrement

--- expansions/knight-errant/attributes/ability_gunsmith/actionables/investigate.js
+++ expansions/knight-errant/attributes/ability_gunsmith/actionables/investigate.js
@@ -52,6 +52,8 @@ module.exports = function (actionable, game, params) {
 
   };
 
+  rs.modular.attributeDecrement(...arguments);
+
 };
 
 module.exports.TAGS = ["drivable", "roleblockable", "visit"];
