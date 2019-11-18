0686273be7ed58b5f6933ce86f9dc358a68563d5
ChocoParrot <lachocoparrot@gmail.com>
2019-11-18 10:12:13 +0800

    [factor] even-night vigilante

---
+++ expansions/lcn-expansion-2/roles/even_night_vigilante/actionables/shoot.js
@@ -0,0 +1,22 @@
+var lcn = require("../../../../../source/lcn.js");
+
+var rs = lcn.rolesystem;
+
+module.exports = function (actionable, game, params) {
+
+  rs.prototypes.powerfulAttack.reason = "shot by a __Vigilante__";
+
+  var outcome = rs.prototypes.powerfulAttack(...arguments);
+
+  var from = game.getPlayerByIdentifier(actionable.from);
+
+  if (!outcome) {
+    var to = game.getPlayerByIdentifier(actionable.to);
+
+    game.addMessage(from, ":exclamation: Your target could not be attacked last night!");
+
+  };
+
+};
+
+module.exports.TAGS = ["drivable", "roleblockable", "visit"];
