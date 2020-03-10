d2d9d58efbc0d20628a60c65125dc5996e13ff22
ChocoParrot <lachocoparrot@gmail.com>
2020-03-10 20:46:18 +0800

    [refactor] added CNB link

--- source/systems/commands_unaffiliated/link.js
+++ source/systems/commands_unaffiliated/link.js
@@ -1,5 +1,5 @@
 module.exports = async function (message, params, config) {
 
-  await message.channel.send(":rose: Come join the official LCN Mafia community!\n\n:one: La Cosa Nostra - https://discord.gg/9ecwAR9\n:two: LCN: Retribution - https://discord.gg/4gaX8ST\n:three: Friends of Cosa Nostra - https://discord.gg/8qdYbZS");
+  await message.channel.send(":rose: Come join the official LCN Mafia community!\n\n:one: La Cosa Nostra - https://discord.gg/9ecwAR9\n:two: LCN: Retribution - https://discord.gg/4gaX8ST\n:three: Friends of Cosa Nostra - https://discord.gg/AfVXbWc\n:four: Cosa Nostra Backstabbers - https://discord.gg/gDHPQMR");
 
 };
